fc6d869704ab2806ba8531c7a1c5ee990335bb87
krindioom <47291437+krindioom@users.noreply.github.com>
2023-12-15 18:14:47 +0700

    закомментил методы препятствий

---
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerObstacleVisitor))]
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    private PlayerObstacleVisitor _playerObstacleVisitor;
+
+    private void Awake()
+    {
+        _playerObstacleVisitor = GetComponent<PlayerObstacleVisitor>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Obstacle component))
+        {
+           // component.AcceptEnter(_playerObstacleVisitor);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Obstacle component))
+        {
+            //component.AcceptExit(_playerObstacleVisitor);
+        }
+    }
+}
